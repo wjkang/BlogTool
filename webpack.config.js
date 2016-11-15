dd2374787f43abe8985a6c6d6589425a8a68948a
Administrator <977865769@qq.com>
2016-11-15 17:33:11 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    entry: {
+        background: './background.js',
+        content:'./content.js',
+        jquery:["jquery"]
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'js/[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CommonsChunkPlugin({
+            name: ["common","jquery","load"],
+            minChunks:2
+        }),
+         new CopyWebpackPlugin([
+            { from: 'manifest.json'},
+            { from: 'icon.png'},
+            { from: 'background.html'},
+         ])
+    ]
+};
