12468d988d77bd7113d4b6102d07a8437c4ee247
Administrator <977865769@qq.com>
2016-11-15 18:19:25 +0800

    update

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 /**
  * Created by Administrator on 2016/11/15.
  */
+"use strict";
+import $ from 'jquery';
--- content.js
+++ content.js
@@ -1,3 +1,8 @@
 /**
  * Created by Administrator on 2016/11/15.
  */
+"use strict";
+import $ from 'jquery';
+
+console.log(1212);
+
